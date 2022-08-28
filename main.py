6c134bc7307e4118adba21c155a1988a9bcda99a
PaSeRouS <paser96@mail.ru>
2022-08-28 22:31:42 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ import telegram
 from environs import Env
 
 
+log = logging.getLogger(__file__)
+
+
+class TelegramLogHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.bot = bot
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+
+        self.bot.send_message(
+            chat_id=self.chat_id,
+            text=log_entry
+        )
+
+
 if __name__ == '__main__':
     env = Env()
     env.read_env()
@@ -14,7 +32,13 @@ if __name__ == '__main__':
     tg_token = env('TG_TOKEN')
     chat_id = env('TG_CHAT_ID')
 
+    logging.basicConfig(level=logging.INFO)
+
     bot = telegram.Bot(token=tg_token)
+
+    log.addHandler(
+        TelegramLogHandler(bot, chat_id)
+    )
 
     url = 'https://dvmn.org/api/long_polling/'
 
@@ -24,6 +48,7 @@ if __name__ == '__main__':
 
     params = {}
 
+    log.info('Бот сейчас запущен.')
     while True:
         try:
             response = requests.get(
@@ -35,8 +60,10 @@ if __name__ == '__main__':
 
             review_data = response.json()
             if review_data['status'] == 'timeout':
+                log.debug('Новых проверок нет.')
                 params = {'timestamp': review_data['timestamp_to_request']}
             elif review_data['status'] == 'found':
+                log.debug('сть новая проверка.')
                 params = {'timestamp': review_data['last_attempt_timestamp']}
 
                 for attempt in review_data['new_attempts']:
@@ -54,9 +81,11 @@ if __name__ == '__main__':
                     text=message
                 )
 
-            logging.warning('Предупреждение, что-то могло сломаться')
-
         except requests.exceptions.ReadTimeout:
-            continue
+            log.warning('Ошибка времени запроса. Продолжайте пробовать...')
         except requests.exceptions.ConnectionError:
+            log_message = 'Ошибка при подключении к интернету. '
+            log_message += 'Следующая попытка через 15 минут.'
+
+            log.warning(log_message)
             sleep(900)
